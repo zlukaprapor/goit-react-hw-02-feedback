b5f0005061e09c33443a5c4859c01a31bc483e3d
Oleksii Tkachenko <zlukapraporr@gmail.com>
2021-12-21 12:24:58 +0200

    add total

--- src/component/feedback/FeedbackOptions.js
+++ src/component/feedback/FeedbackOptions.js
@@ -1,22 +1,43 @@
-import React from "react";
+import React from 'react';
 
 class FeedbackOptions extends React.Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
+    total: 0,
   };
 
   hendleGood = () => {
     this.setState((prevState) => {
-      console.log(prevState);
       return {
         good: prevState.good + 1,
       };
     });
   };
-  hendleNeutral = () => {};
-  hendleBad = () => {};
+  hendleNeutral = () => {
+    this.setState((prevState) => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+  hendleBad = () => {
+    this.setState((prevState) => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const total = this.countTotalFeedback();
+    return total ? ((this.state.good * 100) / total).toFixed(0) : 0;
+  };
+
   render() {
     return (
       <div>
@@ -39,8 +60,8 @@ class FeedbackOptions extends React.Component {
             <li>Good:{this.state.good}</li>
             <li>Neutral:{this.state.neutral}</li>
             <li>Bad:{this.state.bad}</li>
-            <li>Total:</li>
-            <li>Positive feedback:</li>
+            <li>Total:{this.countTotalFeedback()}</li>
+            <li>Positive feedback:{this.countPositiveFeedbackPercentage()}</li>
           </ul>
         </div>
       </div>
